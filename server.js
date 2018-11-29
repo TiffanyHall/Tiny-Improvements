0df8c7e6dcd2eb719b158f3c71cbbf76e1e3bba1
Calithea <tiffanyhall87@gmail.com>
2018-11-29 12:42:45 -0500

    adds server.js info

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //mlablink/
-mongoose.connect('mongodb://localhost/Kudos', { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Kudos';
 
+mongoose.connect(MONGODB_URI, { useNewUrlParser : true}); 
 
 require('./routes/api-routes')(app);
-
 
 app.listen(PORT, function() {
   console.log(`App running on port ${PORT}`);
